b80ae08e067176cd74767bf3062a5ef4d93e1bba
kbr__ <kubrakosee34@gmail.com>
2024-09-17 17:22:42 +0300

    devops-001 eklendi.

---
+++ src/main/java/com/kubrakosee/devops001/Devops001Application.java
@@ -0,0 +1,13 @@
+package com.kubrakosee.devops001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubrakosee/devops001/Devops001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubrakosee.devops001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
