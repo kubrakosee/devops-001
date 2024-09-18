a88e5fe55e39920ac9cd9605d50ca2b8d94d1cbf
kbr__ <kubrakosee34@gmail.com>
2024-09-18 12:57:09 +0300

    Dockerfile dosyası eklendi

--- src/test/java/com/kubrakosee/devops001/AppMainTests.java
+++ src/test/java/com/kubrakosee/devops001/AppMainTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Devops001ApplicationTests {
+class AppMainTests {
 
 	@Test
 	void contextLoads() {
